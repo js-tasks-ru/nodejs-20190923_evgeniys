6c08fa2a815e41037877733b16b27b7e675c1035
Evgeniy Sorogin <sorogin@gmail.com>
2019-10-17 18:26:06 +0200

    add solution 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,27 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+      if (path.basename(pathname) !== pathname) {
+        res.statusCode = 400;
+        res.end('Invalid path');
+        return;
+      }
+      fs.access(filepath, fs.constants.F_OK, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('File does not exist');
+          return;
+        }
+      });
+      fs.unlink(filepath, (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end('cannot delete file');
+          return;
+        }
+        res.statusCode = 200;
+        res.end('File has been deleted');
+      });
 
       break;
 
